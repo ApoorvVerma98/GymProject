d679730d4267f79d43e6f390c985e2cf4ddf612b
ApoorvVerma98 <apoorv.verma9@gmail.com>
2023-04-20 19:56:12 +0530

    1final deploy

--- Nobelium-project1-deploy-render/src/App.jsx
+++ Nobelium-project1-deploy-render/src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/home/Home";
-import Login from "./Pages/login/Login";
+import Login from "./Pages/Login/Login";
 import About from "./Pages/AboutUs/About-Us";
 import Pricing from "./Pages/pricing/Pricing";
 import TrainingPrograms from "./Pages/training/TrainingProgram";
-import Register from "./Pages/register/Register";
+import Register from "./Pages/Register/Register";
 
 const App = () => {
   return (
